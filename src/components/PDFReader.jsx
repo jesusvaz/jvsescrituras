c57073cf5af67374c14b3aa16fe32f10345945f6
jesusvaz <jesusvaz@gmail.com>
2021-11-01 12:25:48 -0600

    Update PDFReader.jsx

--- src/components/PDFReader.jsx
+++ src/components/PDFReader.jsx
@@ -28,7 +28,7 @@ const PDFReader = () => {
           numPages={numPages}
           pageNumber={pageNumber}
           setPageNumber={setPageNumber}
-          file="/assets/docs/file-sample.pdf"
+          file="/public/assets/docs/file-sample.pdf
         />
         <Document
           file="/assets/docs/file-sample.pdf"
